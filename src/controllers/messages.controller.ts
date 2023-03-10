f360de1942b591604b87ee2fa1d1b200b460b87e
Jonathan Correa <jdcorrea918@gmail.com>
2023-03-10 11:18:05 -0500

    fix: retrieve createdBy info in message data

--- src/controllers/messages.controller.ts
+++ src/controllers/messages.controller.ts
@@ -4,16 +4,22 @@ import { IMessage } from '../interfaces/IMessage';
 import { IRequest } from '../interfaces/IRequest';
 
 import Message from '../models/Message';
+import User from '../models/User';
 
 export const create = async (
   req: IRequest,
   res: Response
 ): Promise<void | Response> => {
   try {
-    const message: IMessage = new Message(req.body);
+    let message: IMessage = new Message(req.body);
     message.createdBy = req.user?._id;
 
     await message.save();
+    message = message.toObject();
+
+    message.createdBy = (
+      await User.findById(req.user?._id).select('fullName nickname email createdAt _id').lean()
+    ) as any;
 
     return res.status(200).json(message);
   } catch (error) {
